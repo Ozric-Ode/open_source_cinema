ccea87a5bde975a13bc897fa5ee3be1d557374ed
Monikinderjit Singh <52850691+Monik09@users.noreply.github.com>
2021-01-31 01:34:15 +0530

    Update user_screen.dart

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -6,61 +6,65 @@ class UserScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-       appBar: AppBar(
+      appBar: AppBar(
         title: Text('Open Source Cinema'),
         backgroundColor: Colors.deepPurple,
       ),
-      body:Container(
-        child: Column(
+      body:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            
-                 Row(
-                   children: <Widget>[
-
-                  
-                     SizedBox(width: 125,),
-                      Container(
+            SizedBox(height:50),
+            // Row(
+            //   crossAxisAlignment: CrossAxisAlignment.center,
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: <Widget>[
+               
+                Container(
                   height: 100.0,
                   width: 100.0,
-                 
                   decoration: BoxDecoration(
-                border: Border.all(
-                  width: 5,
-                ),
-              ),
-              child:Center(child:Image.network('https://googleflutter.com/sample_image.jpg'),),
-            ),
-                  //  BoxDecoration(
-                  //   image: DecorationImage(
-                  //     image:AssetImage("FlutterLogo(size: 56.0)"),
-                     
-                  //     fit:BoxFit.fill,
-                  //   ),
-                  //   shape: BoxShape.circle,
-                  // ),
-             //   ),
-           
-                   ],
-                 ),
-                 SizedBox(height: 10,),
-               Text(
-                 "Hi, uesrname",
-                   style: TextStyle(
-                    // Theme.of(context).textTheme.headline4,
-                    fontSize: 34.0,
-                  //  fontWeight: FontWeight.w100 ,
-                    foreground: Paint()
-                    ..color=Colors.teal,
-                   
+                    border: Border.all(
+                      width: 0.5,
+                    ),
                   ),
-               ),
-                SizedBox(height: 10,),
-           Flexible(
-             child:RepoList(),
-             ),
+                  child: Center(
+                    child: Image.network(
+                        'https://googleflutter.com/sample_image.jpg'),
+                  ),
+                ),
+                //  BoxDecoration(
+                //   image: DecorationImage(
+                //     image:AssetImage("FlutterLogo(size: 56.0)"),
+
+                //     fit:BoxFit.fill,
+                //   ),
+                //   shape: BoxShape.circle,
+                // ),
+                //   ),
+            //   ],
+            // ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Hi, username",
+              style: TextStyle(
+                // Theme.of(context).textTheme.headline4,
+                fontSize: 26.0,
+                //  fontWeight: FontWeight.w100 ,
+                foreground: Paint()..color = Colors.teal,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Flexible(
+              child: RepoList(),
+            ),
           ],
         ),
-      ),
+      // ),
     );
   }
 }
